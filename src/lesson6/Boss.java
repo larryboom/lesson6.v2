6dccac4eb3ee2821aaa27d81a8c5002249ab640c
Vlad Fortocnik <vladfortocnik@gmail.com>
2019-11-15 16:41:39 +0200

    Initial commit

---
+++ src/lesson6/Boss.java
@@ -0,0 +1,16 @@
+
+
+
+
+public class Boss {
+
+
+    Worker[] workers = {new Worker(1), new Worker(2), new Worker(3)};
+
+    void work(String task) {
+        for (Worker worker : workers) {
+            if (!worker.busy)
+                worker.work(task);
+        }
+    }
+}
---
+++ src/lesson6/Main.java
@@ -0,0 +1,12 @@
+
+
+
+public class Boss {
+    public static void main(String[] args) {
+
+        // форматування коду
+
+        String str = " dsljsdfl";
+        String strs = new String("dsfsldj");
+    }
+}
